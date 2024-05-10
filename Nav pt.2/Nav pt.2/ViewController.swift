7b404ac36d7d010e4bcf5b9339d961807c32c857
Divina Vidal <divinavidal@Divinas-MacBook-Pro.local>
2024-05-09 23:07:36 -0700

    adding nav

---
+++ Nav pt.2/Nav pt.2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Nav pt.2
+//
+//  Created by Alejandra Padilla on 3/30/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
